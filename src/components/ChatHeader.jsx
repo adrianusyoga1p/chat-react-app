ca0a1416e3a94f42656cad8a506b9d86a98061f4
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-19 13:26:47 +0700

    chat header component

---
+++ src/components/ChatHeader.jsx
@@ -0,0 +1,20 @@
+import Avatar from "@/assets/avatar.png";
+
+const ChatHeader = ({ currentUser, chatId, user }) => {
+  return (
+    <div className="sticky w-full rounded-lg bg-white p-2.5 top-0 shadow-md">
+      <div className="flex gap-4 items-center">
+        <img src={Avatar} alt="avatar" className="h-11 w-11" />
+        <div>
+          <p className="font-medium">{user.username}</p>
+          <div className="flex gap-1 items-center">
+            <div className="w-2 h-2 rounded-full bg-green-600"></div>
+            <span className="text-green-600 text-sm">Online</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;

dfda98d1f9ad59d7aebdbca447c927cf282967ad
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-19 13:27:24 +0700

    loading page

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='absolute inset-0 z-50 bg-white w-full h-full text-gray-700 text-xl flex items-center justify-center'>Loading..</div>
+  )
+}
+
+export default Loading

71dfd8259f99979be83d3f592938b5d6eb2828ca
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-19 13:29:06 +0700

    add chatstore

---
+++ src/store/chatStore.js
@@ -0,0 +1,47 @@
+import { create } from "zustand";
+import { userStore } from "./userStore";
+
+export const chatStore = create((set) => ({
+  chatId: null,
+  user: null,
+  isCurrentUserBlocked: false,
+  isReceiverBlocked: false,
+  changeChat: (chatId, user) => {
+    const currentUser = userStore.getState().currentUser;
+
+    if (user.blocked.includes(currentUser.id)) {
+      return set({
+        chatId,
+        user: null,
+        isCurrentUserBlocked: true,
+        isReceiverBlocked: false,
+      });
+    } else if (currentUser.blocked.includes(user.id)) {
+      return set({
+        chatId,
+        user: user,
+        isCurrentUserBlocked: false,
+        isReceiverBlocked: true,
+      });
+    } else {
+      return set({
+        chatId,
+        user,
+        isCurrentUserBlocked: false,
+        isReceiverBlocked: false,
+      });
+    }
+  },
+
+  changeBlock: () => {
+    set((state) => ({ ...state, isReceiverBlocked: !state.isReceiverBlocked }));
+  },
+  resetChat: () => {
+    set({
+      chatId: null,
+      user: null,
+      isCurrentUserBlocked: false,
+      isReceiverBlocked: false,
+    });
+  },
+}));

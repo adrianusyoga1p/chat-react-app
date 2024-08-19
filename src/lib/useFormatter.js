f34314b74a175c362dfe2afc45ef8edb78914f7c
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-19 13:28:13 +0700

    useFormatter

---
+++ src/lib/useFormatter.js
@@ -0,0 +1,19 @@
+import { clsx } from "clsx";
+import Swal from "sweetalert2";
+import { twMerge } from "tailwind-merge";
+
+export function useFormatter() {
+  const formatPrice = (price = 0) => {
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+      maximumFractionDigits: 0,
+    }).format(parseFloat((price || "0").toString()) || 0);
+  };
+
+  const cn = (...inputs) => {
+    return twMerge(clsx(inputs));
+  };
+
+  return { formatPrice, cn };
+}

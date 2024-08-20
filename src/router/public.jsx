4761b499d61220d0fe03f5cfb6d85e8627b64505
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-20 11:57:08 +0700

    add public routes

---
+++ src/router/public.jsx
@@ -0,0 +1,34 @@
+import { userStore } from "@/store/userStore";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../firebase";
+
+export const PublicRoute = ({ children }) => {
+  const { currentUser, fetchUserInfo, isLoading } = userStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        fetchUserInfo(user.uid);
+        navigate("/chat-app", { replace: true });
+      } else {
+        return navigate("/", {replace: true});
+      }
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo, navigate]);
+
+  useEffect(() => {
+    if (!currentUser && !isLoading) {
+      return navigate("/", { replace: true });
+    }
+  }, [currentUser, navigate, isLoading]);
+
+
+  return !currentUser ? children : null;
+};

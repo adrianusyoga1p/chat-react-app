97229f4ca7dc317b9877eb5cebda3432b1169e91
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-20 11:55:24 +0700

    adjust chat header

--- src/components/ChatHeader.jsx
+++ src/components/ChatHeader.jsx
@@ -4,9 +4,9 @@ const ChatHeader = ({ currentUser, chatId, user }) => {
   return (
     <div className="sticky w-full rounded-lg bg-white p-2.5 top-0 shadow-md">
       <div className="flex gap-4 items-center">
-        <img src={Avatar} alt="avatar" className="h-11 w-11" />
+        <img src={user?.avatar} alt="avatar" className="h-11 w-11" />
         <div>
-          <p className="font-medium">{user.username}</p>
+          <p className="font-medium">{user?.username}</p>
           <div className="flex gap-1 items-center">
             <div className="w-2 h-2 rounded-full bg-green-600"></div>
             <span className="text-green-600 text-sm">Online</span>

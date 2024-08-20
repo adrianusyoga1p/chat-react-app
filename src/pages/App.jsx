4c7c358bcb632966b18e83267993cdef155caaa5
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-20 11:56:31 +0700

    adjust routes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,13 +2,31 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./Login/Login";
 import Home from "./Home";
 import Register from "./Register/Register";
+import GuardedRoute from "@/router/guard";
+import { PublicRoute } from "@/router/public";
+import NotFound from "./NotFound/NotFound";
 
 const App = () => {
   return (
     <Routes>
-      <Route element={<Login />} path="/" />
-      <Route element={<Register />} path="/register" />
-      <Route element={<Home />} path="/chat-app" />
+      <Route
+        path="/"
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+      />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/chat-app"
+        element={
+          <GuardedRoute>
+            <Home />
+          </GuardedRoute>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };

ba3d7be60ec7aa66b81faade555df5e200d75c94
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-21 15:58:24 +0700

    adjust chat card

--- src/components/CurrentChat.jsx
+++ src/components/CurrentChat.jsx
@@ -2,7 +2,7 @@ const CurrentChat = ({ message, createdAt }) => {
   return (
     <div className="p-4 max-w-72 w-fit bg-primary text-white ml-auto rounded-xl rounded-br-sm relative">
       {message}
-      <div className="text-[9px] text-right">{createdAt}</div>
+      <div className="text-[9px] text-right pt-1">{createdAt}</div>
     </div>
   );
 };
--- src/components/OtherChat.jsx
+++ src/components/OtherChat.jsx
@@ -2,7 +2,7 @@ const OtherChat = ({ message, createdAt }) => {
   return (
     <div className="p-4 max-w-72 w-fit bg-cyan-900 text-white mr-auto rounded-xl rounded-bl-sm relative">
       {message}
-      <div className="text-[9px] text-right">{createdAt}</div>
+      <div className="text-[9px] text-right pt-1">{createdAt}</div>
     </div>
   );
 };

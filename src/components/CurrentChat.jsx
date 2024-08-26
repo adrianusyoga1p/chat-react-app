baa0a5ce6f0cff8a8c827533d0c9dd9b0c3c925e
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-26 16:43:23 +0700

    add image format

--- src/components/CurrentChat.jsx
+++ src/components/CurrentChat.jsx
@@ -1,8 +1,9 @@
-const CurrentChat = ({ message, createdAt }) => {
+const CurrentChat = ({ message, createdAt, image }) => {
   return (
     <div className="p-4 max-w-72 w-fit bg-primary text-white ml-auto rounded-xl rounded-br-sm relative">
+      {image && <img src={image} className={`${message && 'mb-2'} rounded`} />}
       {message}
-      <div className="text-[9px] text-right pt-1">{createdAt}</div>
+      <div className="text-[9px] text-right mt-1.5">{createdAt}</div>
     </div>
   );
 };
--- src/components/OtherChat.jsx
+++ src/components/OtherChat.jsx
@@ -1,8 +1,9 @@
-const OtherChat = ({ message, createdAt }) => {
+const OtherChat = ({ message, createdAt, image }) => {
   return (
     <div className="p-4 max-w-72 w-fit bg-cyan-900 text-white mr-auto rounded-xl rounded-bl-sm relative">
+      {image && <img src={image} className={`${message && 'mb-2'} rounded`} />}
       {message}
-      <div className="text-[9px] text-right pt-1">{createdAt}</div>
+      <div className="text-[9px] text-right mt-1.5">{createdAt}</div>
     </div>
   );
 };

a13822bcd5fc557ad9ffea79c27af250c4dbdce5
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-08-27 09:52:46 +0700

    add formating date

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -9,6 +9,7 @@ import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
 import { useFormatter } from "@/lib/useFormatter";
 import moment from "moment";
+import Loading from "./Loading";
 
 const Chat = () => {
   const [chat, setChat] = useState(null);
@@ -31,73 +32,93 @@ const Chat = () => {
   }, [chatId]);
 
   const { formatDate } = useFormatter();
-  // const formatDates = (date) => {
-  //   const today = moment().startOf("day");
-  //   const messageDate = moment.unix(date.seconds);
-  //   if (messageDate.isSame(today, "day")) {
-  //     return "Today";
-  //   } else if (messageDate.isSame(today.clone().subtract(1, "day"), "day")) {
-  //     return "Yesterday";
-  //   } else {
-  //     return messageDate.format("MMMM D, YYYY");
-  //   }
-  // };
+  const formatingDate = (date) => {
+    const today = moment().startOf("day");
+    let messageDate;
+
+    if (typeof date === "object" && date.seconds) {
+      messageDate = moment.unix(date.seconds);
+    } else {
+      messageDate = moment(date); // For ISO date string or other formats
+    }
+
+    if (!messageDate.isValid()) {
+      return "Invalid Date";
+    }
+
+    if (messageDate.isSame(today, "day")) {
+      return "Today";
+    } else if (messageDate.isSame(today.clone().subtract(1, "day"), "day")) {
+      return "Yesterday";
+    } else {
+      return messageDate.format("MMMM D, YYYY");
+    }
+  };
+
+  const formatDates = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+    const day = ("0" + date.getDate()).slice(-2);
+    return `${year}-${month}-${day}`;
+  };
+
+  const groupMessagesByDate = (chat) => {
+    return chat?.reduce((acc, message) => {
+      const date = formatDates(message.createdAt.seconds);
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+      acc[date].push({
+        createdAt: message.createdAt,
+        text: message.text,
+        senderId: message.senderId,
+        img: message.img,
+      });
+      return acc;
+    }, {});
+  };
+
+  const formattedMessages = {
+    createdAt: chat?.messages[0].createdAt,
+    newMessages: groupMessagesByDate(chat?.messages),
+  };
+
+  console.log(formattedMessages.newMessages);
+
+  if (!formattedMessages || !formattedMessages?.newMessages) {
+    return <Loading />;
+  }
 
   return (
     <div className="p-6 bg-soft_primary w-[calc(100%-320px)] max-h-screen relative flex-col flex justify-between">
       <ChatHeader user={user} chatId={chatId} currentUser={currentUser} />
-      <div className="relative pt-4 mt-auto h-fit max-h-full overflow-y-auto scroll scroll-auto space-y-4">
-        {/* {formattedMessages.newMessages.map((date) => (
-          <div key={date}>
-            <h3>Messages for {formatDates(date)}</h3>
-            {formattedMessages?.newMessages[date].map((message, index) => {
-              const date = new Date(message.createdAt.seconds * 1000);
-              const formattedDate = date.toLocaleString();
-              return (
-                <div key={index} style={{ marginBottom: "10px" }}>
-                  <p>
-                    <strong>Date:</strong> {formattedDate}
-                  </p>
-                  {message.text && (
-                    <p>
-                      <strong>Text:</strong> {message.text}
-                    </p>
-                  )}
-                  {message.img && (
-                    <img
-                      src={message.img}
-                      alt="Message"
-                      style={{ maxWidth: "100%", height: "auto" }}
-                    />
-                  )}
-                </div>
+      <div className="relative pt-4 mt-auto h-fit max-h-full overflow-y-auto scroll scroll-auto">
+        {Object.keys(formattedMessages?.newMessages).map((date, i) => (
+          <div className="space-y-4" key={i}>
+            <div className="sticky p-2 text-sm rounded bg-white top-0 mx-auto w-fit z-20">
+              {formatingDate(date)}
+            </div>
+            {formattedMessages?.newMessages[date].map((msg, index) => {
+              return msg?.senderId === currentUser?.id ? (
+                <CurrentChat
+                  message={msg.text}
+                  createdAt={formatDate(msg.createdAt.toString())}
+                  key={index}
+                  image={msg.img}
+                />
+              ) : (
+                <OtherChat
+                  message={msg.text}
+                  key={index}
+                  createdAt={formatDate(msg.createdAt.toString())}
+                  image={msg.img}
+                />
               );
             })}
           </div>
-        ))} */}
-        {chat?.messages.map((msg, i) => (
-          <div key={i}>
-            {/* <div className="sticky p-2 text-sm rounded bg-white top-0 mx-auto w-fit z-20">
-              {formatDates(msg.createdAt)}
-            </div> */}
-            {msg?.senderId === currentUser?.id ? (
-              <CurrentChat
-                message={msg.text}
-                createdAt={formatDate(msg.createdAt.toString())}
-                key={msg.senderId}
-                image={msg.img}
-              />
-            ) : (
-              <OtherChat
-                message={msg.text}
-                key={msg.senderId}
-                createdAt={formatDate(msg.createdAt.toString())}
-                image={msg.img}
-              />
-            )}
-          </div>
         ))}
-        <div ref={endRef}></div>
+        <div className="mb-4" ref={endRef}></div>
       </div>
       <InputMessage currentUser={currentUser} chatId={chatId} user={user} />
     </div>
